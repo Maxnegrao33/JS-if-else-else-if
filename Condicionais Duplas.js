245f79b3f86b8f08192417779a972ddf9e9e1a49
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-11-28 18:21:30 -0300

    Add files via upload

---
+++ Condicionais Duplas.js
@@ -0,0 +1,23 @@
+let idade = 55;
+
+if(idade <18) {
+    console.log("Você é uma criança.");
+} else if(idade >18 || idade <60) {
+    console.log("Você é um adulto.");
+} else if(idade >60) {
+    console.log("Você é um idoso.");
+}
+
+
+//Exemplificando uma condição booleana
+//Toda a condicional retorna um boolean
+let ok = true;
+    if(ok) {
+        console.log("Entrou no if");
+    } else {
+        console.log("Não entrou no if");
+    }
+
+
+
+
